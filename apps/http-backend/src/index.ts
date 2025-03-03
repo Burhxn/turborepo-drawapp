f95d2be41003ed0798efcc638590de06084eac03
burhxn <burhxnn@gmail.com>
2025-03-03 23:08:33 +0530

    added backend-common

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,18 +1,35 @@
 import express from "express"
 import jwt from "jsonwebtoken";
-import { JWT_SECRET } from "./config";
+import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleware } from "./middleware";
+import { CreateUserSchema, SigninSchema, CreateRoomSchema } from "@repo/common/types"
 
 
 const app = express();
 
 
 app.post("/signup", (req, res) => {
+
+    const data = CreateUserSchema.safeParse(req.body);
+    if (!data.success) {
+        res.json({
+            message: "Incorrect inputs"
+        })
+        return;
+    }
+
     res.json({
-        userID : 123
+        userID: 123
     })
 })
 app.post("/signin", (req, res) => {
+    const data = SigninSchema.safeParse(req.body);
+    if (!data.success) {
+        res.json({
+            message: "Incorrect inputs"
+        })
+        return;
+    }
 
 
     const userId = 1;
@@ -25,8 +42,15 @@ app.post("/signin", (req, res) => {
 
 })
 app.post("/room", middleware, (req, res) => {
+    const data = CreateRoomSchema.safeParse(req.body);
+    if (!data.success) {
+        res.json({
+            message: "Incorrect inputs"
+        })
+        return;
+    }
     res.json({
-        roomId : 133
+        roomId: 133
     })
 })
 
--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,6 +1,6 @@
 import { WebSocketServer } from "ws";
 import jwt, { JwtPayload } from "jsonwebtoken";
-import { JWT_SECRET } from "./config";
+import { JWT_SECRET } from "@repo/backend-common/config";
 
 
 const wss = new WebSocketServer({ port: 8080 })
@@ -14,8 +14,14 @@ wss.on("connection", function connection(ws, request) {
 
     const queryParams = new URLSearchParams(url.split('?')[1]);
     const token = queryParams.get('token') || "";
-    const decoded = jwt.verify(token, JWT_SECRET)
-    if (!decoded || !(decoded as JwtPayload).userId) {
+    const decoded = jwt.verify(token, JWT_SECRET);
+
+    if(typeof decoded === "string"){
+        ws.close();
+        return;
+    }
+
+    if (!decoded || !decoded.userId) {
         ws.close();
         return
     }
