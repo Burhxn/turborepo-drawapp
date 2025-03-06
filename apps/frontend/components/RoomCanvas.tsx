db694bc65c988cf7ada93cd2facd3230084579b1
burhxn <burhxnn@gmail.com>
2025-03-07 01:34:12 +0530

    added circle

--- apps/frontend/components/RoomCanvas.tsx
+++ apps/frontend/components/RoomCanvas.tsx
@@ -5,31 +5,34 @@ import { initDraw } from "@/draw";
 import { useEffect, useRef, useState } from "react";
 import { Canvas } from "./Canvas";
 
-export function RoomCanvas({roomId}: {roomId: string}) {
-    const [socket, setSocket] = useState<WebSocket | null>(null);
+export function RoomCanvas({ roomId }: { roomId: string }) {
+  const [socket, setSocket] = useState<WebSocket | null>(null);
 
-    useEffect(() => {
-        const ws = new WebSocket(`${WS_URL}?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI4NGRlMzU1My0yNThmLTQ3YmMtODdlYy1iYTI5MWIwODg0YmEiLCJpYXQiOjE3MzcyMTczMjF9.b2Q3B-RUDAV0equhOjET7Hyl1AQaBf1FW4szk6mvGxU`)
+  useEffect(() => {
+    const ws = new WebSocket(
+      `${WS_URL}?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI0ZDVlODk1OC04MzdjLTQ5ZDctODJjNS1mNDQ3OWJmODE0OGEiLCJpYXQiOjE3NDEyODQwNjJ9.8RSt0QSB25qADgqMIbgq-37vYXe0wIr7nAQquTrJuLQ`
+    );
 
-        ws.onopen = () => {
-            setSocket(ws);
-            const data = JSON.stringify({
-                type: "join_room",
-                roomId
-            });
-            console.log(data);
-            ws.send(data)
-        }
-        
-    }, [])
-   
-    if (!socket) {
-        return <div>
-            Connecting to server....
-        </div>
-    }
+    ws.onopen = () => {
+      setSocket(ws);
+      const data = JSON.stringify({
+        type: "join_room",
+        roomId,
+      });
+      console.log(data);
+      ws.send(data);
+    };
+  }, []);
 
-    return <div>
-        <Canvas roomId={roomId} socket={socket} />
+  if (!socket) {
+    return <div>Connecting to server....</div>;
+  }
+
+  return (
+    <div>
+      <Canvas roomId={roomId} socket={socket} />
+
+      hi thereeee
     </div>
+  );
 }
